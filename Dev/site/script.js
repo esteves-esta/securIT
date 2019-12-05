6d2bc50b9196da8f9e5c08ab6069a2d05354a5a7
rayssawoods <rayssa.hcl@gmail.com>
2019-12-05 11:21:52 -0300

    algumas alterações no PPT

--- Dev/site/script.js
+++
@@ -1,7 +0,0 @@
-function isNull(element) {
-    return element == null || element === false || (typeof element === typeof Object() && element.length === 0) || element == '';
-}
-
-module.exports = {
-    isNull: isNull
-};
